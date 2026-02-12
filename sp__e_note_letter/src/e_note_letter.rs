0de66f821ec4ee8fb55c531b16ba07bb5fad43be
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-02-12 22:50:14 +0000

    sp__e_note_letter crate added

---
+++ sp__e_note_letter/src/e_note_letter.rs
@@ -0,0 +1,43 @@
+use std::fmt::Display;
+
+use sp__t_keyboard_friendly_name::tKeyboardFriendlyName;
+
+#[derive(Clone, Copy)]
+pub enum eNoteLetter {
+    C,
+    D,
+    E,
+    F,
+    G,
+    A,
+    B,
+}
+
+
+impl Display for eNoteLetter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::C => write!(f, "{}", "C"),
+            Self::D => write!(f, "{}", "D"),
+            Self::E => write!(f, "{}", "E"),
+            Self::F => write!(f, "{}", "F"),
+            Self::G => write!(f, "{}", "G"),
+            Self::A => write!(f, "{}", "A"),
+            Self::B => write!(f, "{}", "B"),
+        }
+    }
+}
+
+impl tKeyboardFriendlyName for eNoteLetter {
+    fn get_keyboard_friendly_name(&self) -> &'static str {
+        match self {
+            Self::C => "c",
+            Self::D => "d",
+            Self::E => "e",
+            Self::F => "f",
+            Self::G => "g",
+            Self::A => "a",
+            Self::B => "b",
+        }
+    }
+}
