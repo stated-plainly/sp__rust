7f9ec0db605334855a9de997c598655c20a0e796
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-02-12 22:55:55 +0000

    sp__e_note_modifier crate added

---
+++ sp__e_note_modifier/src/e_note_modifier.rs
@@ -0,0 +1,37 @@
+use std::fmt::Display;
+
+use sp__t_keyboard_friendly_name::tKeyboardFriendlyName;
+
+#[derive(Clone, Copy)]
+pub enum eNoteModifier {
+    DoubleFlat,
+    Flat,
+    Natural,
+    Sharp,
+    DoubleSharp,
+}
+
+
+impl Display for eNoteModifier {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::DoubleFlat => write!(f, "{}", "𝄫"),
+            Self::Flat => write!(f, "{}", "♭"),
+            Self::Natural => write!(f, "{}", "♮"),
+            Self::Sharp => write!(f, "{}", "♯"),
+            Self::DoubleSharp => write!(f, "{}", "𝄪"),
+        }
+    }
+}
+
+impl tKeyboardFriendlyName for eNoteModifier {
+    fn get_keyboard_friendly_name(&self) -> &'static str {
+        match self {
+            Self::DoubleFlat => "bb",
+            Self::Flat => "b",
+            Self::Natural => "",
+            Self::Sharp => "s",
+            Self::DoubleSharp => "ss",
+        }
+    }
+}
---
+++ sp__e_note_modifier/src/lib.rs
@@ -0,0 +1,4 @@
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+mod e_note_modifier;
+pub use e_note_modifier::eNoteModifier;
