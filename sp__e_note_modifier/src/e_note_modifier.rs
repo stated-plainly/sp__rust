562ecc6000801edfe54a5d793347719fa4ce9f42
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-02-13 00:10:52 +0000

    sp__e_note_modifier :: try_flatten and try_sharpen methods added

--- sp__e_note_modifier/src/e_note_modifier.rs
+++ sp__e_note_modifier/src/e_note_modifier.rs
@@ -1,5 +1,7 @@
 use std::fmt::Display;
 
+use sp__s_impossible_operation_error::sImpossibleOperationError;
+use sp__t_error::tError;
 use sp__t_keyboard_friendly_name::tKeyboardFriendlyName;
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
@@ -12,6 +14,27 @@ pub enum eNoteModifier {
     DoubleSharp,
 }
 
+impl eNoteModifier {
+    pub fn try_flatten(&self) -> Result<Self, Box<dyn tError>> {
+        match self {
+            Self::DoubleFlat => Err(Box::new(sImpossibleOperationError::new("Cannot flatten a double-flat note"))),
+            Self::Flat => Ok(Self::DoubleFlat),
+            Self::Natural => Ok(Self::Flat),
+            Self::Sharp => Ok(Self::Natural),
+            Self::DoubleSharp => Ok(Self::Sharp),
+        }
+    }
+
+    pub fn try_sharpen(&self) -> Result<Self, Box<dyn tError>> {
+        match self {
+            Self::DoubleFlat => Ok(Self::Flat),
+            Self::Flat => Ok(Self::Natural),
+            Self::Natural => Ok(Self::Sharp),
+            Self::Sharp => Ok(Self::DoubleSharp),
+            Self::DoubleSharp => Err(Box::new(sImpossibleOperationError::new("Cannot sharpen a double-sharp note"))),
+        }
+    }
+}
 
 impl Display for eNoteModifier {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
