533905f15019846deec6af1b4a658cfdfa1ad964
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-02-15 14:04:37 +0000

    sp__e_scale_degree :: String representations moved out to const arrays

--- sp__e_scale_degree/src/e_scale_degree.rs
+++ sp__e_scale_degree/src/e_scale_degree.rs
@@ -1,6 +1,57 @@
 use std::fmt::Display;
 
+use sp__s_impossible_operation_error::sImpossibleOperationError;
 use sp__t_keyboard_friendly_name::tKeyboardFriendlyName;
+
+const DISPLAY_NAMES: &[&str] = &[
+    "1",
+    "2♭",
+    "2",
+    "3♭",
+    "3",
+    "4",
+    "4♯",
+    "5♭",
+    "5",
+    "5♯",
+    "6♭",
+    "6",
+    "7♭",
+    "7",
+    "8",
+    "9♭",
+    "9",
+    "9♯",
+    "11",
+    "11♯",
+    "13♭",
+    "13",
+];
+
+const KEYBOARD_FRIENDLY_NAMES: &[&str] = &[
+    "1",
+    "2b",
+    "2",
+    "3b",
+    "3",
+    "4",
+    "4s",
+    "5b",
+    "5",
+    "5s",
+    "6b",
+    "6",
+    "7b",
+    "7",
+    "8",
+    "9b",
+    "9",
+    "9s",
+    "11",
+    "11s",
+    "13b",
+    "13",
+];
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
 pub enum eScaleDegree {
@@ -29,60 +80,74 @@ pub enum eScaleDegree {
     Thirteen,
 }
 
+impl TryFrom<usize> for eScaleDegree {
+    type Error = sImpossibleOperationError;
+
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::One),
+            1 => Ok(Self::TwoFlat),
+            2 => Ok(Self::Two),
+            3 => Ok(Self::ThreeFlat),
+            4 => Ok(Self::Three),
+            5 => Ok(Self::Four),
+            6 => Ok(Self::FourSharp),
+            7 => Ok(Self::FiveFlat),
+            8 => Ok(Self::Five),
+            9 => Ok(Self::FiveSharp),
+            10 => Ok(Self::SixFlat),
+            11 => Ok(Self::Six),
+            12 => Ok(Self::SevenFlat),
+            13 => Ok(Self::Seven),
+            14 => Ok(Self::Eight),
+            15 => Ok(Self::NineFlat),
+            16 => Ok(Self::Nine),
+            17 => Ok(Self::NineSharp),
+            18 => Ok(Self::Eleven),
+            19 => Ok(Self::ElevenSharp),
+            20 => Ok(Self::ThirteenFlat),
+            21 => Ok(Self::Thirteen),
+            index => Err(sImpossibleOperationError::new(format!("'{}' is not a valid eScaleDegree index", index).as_str())),
+        }
+    }
+}
+
+impl Into<usize> for eScaleDegree {
+    fn into(self) -> usize {
+        let index: usize = self.try_into().expect("The conversion can only fail from the usize -> eScaleDegree side. As we're converting from the eScaleDegree, we know that the conversion will succeed.");
+
+        index
+    }
+}
+
 impl Display for eScaleDegree {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::One => write!(f, "{}", "1"),
-            Self::TwoFlat => write!(f, "{}", "2♭"),
-            Self::Two => write!(f, "{}", "2"),
-            Self::ThreeFlat => write!(f, "{}", "3♭"),
-            Self::Three => write!(f, "{}", "3"),
-            Self::Four => write!(f, "{}", "4"),
-            Self::FourSharp => write!(f, "{}", "4♯"),
-            Self::FiveFlat => write!(f, "{}", "5♭"),
-            Self::Five => write!(f, "{}", "5"),
-            Self::FiveSharp => write!(f, "{}", "5♯"),
-            Self::SixFlat => write!(f, "{}", "6♭"),
-            Self::Six => write!(f, "{}", "6"),
-            Self::SevenFlat => write!(f, "{}", "7♭"),
-            Self::Seven => write!(f, "{}", "7"),
-            Self::Eight => write!(f, "{}", "8"),
-            Self::NineFlat => write!(f, "{}", "9♭"),
-            Self::Nine => write!(f, "{}", "9"),
-            Self::NineSharp => write!(f, "{}", "9♯"),
-            Self::Eleven => write!(f, "{}", "11"),
-            Self::ElevenSharp => write!(f, "{}", "11♯"),
-            Self::ThirteenFlat => write!(f, "{}", "13♭"),
-            Self::Thirteen => write!(f, "{}", "13"),
+        let index: usize = self.clone().into();
+
+        write!(f, "{}", DISPLAY_NAMES[index])
+    }
+}
+
+impl TryFrom<String> for eScaleDegree {
+    type Error = sImpossibleOperationError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        for (index, keyboard_friendly_name) in KEYBOARD_FRIENDLY_NAMES.iter().enumerate() {
+            if value == *keyboard_friendly_name {
+                let scale_degree: Self = index.try_into().expect("The index is auto-generated from the associated KEYBOARD_FRIENDLY_NAMES array, guaranteeing that this conversion will succeed.");
+                
+                return Ok(scale_degree);
+            }
         }
+
+        Err(sImpossibleOperationError::new(format!("'{}' is not a valid String representation of a eScaleDegree", value).as_str()))
     }
 }
 
 impl tKeyboardFriendlyName for eScaleDegree {
     fn get_keyboard_friendly_name(&self) -> String {
-        match self {
-            Self::One => "1",
-            Self::TwoFlat => "2b",
-            Self::Two => "2",
-            Self::ThreeFlat => "3b",
-            Self::Three => "3",
-            Self::Four => "4",
-            Self::FourSharp => "4s",
-            Self::FiveFlat => "5b",
-            Self::Five => "5",
-            Self::FiveSharp => "5s",
-            Self::SixFlat => "6b",
-            Self::Six => "6",
-            Self::SevenFlat => "7b",
-            Self::Seven => "7",
-            Self::Eight => "8",
-            Self::NineFlat => "9b",
-            Self::Nine => "9",
-            Self::NineSharp => "9s",
-            Self::Eleven => "11",
-            Self::ElevenSharp => "11s",
-            Self::ThirteenFlat => "13b",
-            Self::Thirteen => "13",
-        }.to_string()
+        let index: usize = self.clone().into();
+
+        KEYBOARD_FRIENDLY_NAMES[index].to_string()
     }
 }
