120b0850cd5675c7b81bc7ef9eb6a612e544936d
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-12 22:05:40 +0000

    casper (lib) :: sInterLangObject struct added

--- inter_lang/src/ast/s_inter_lang_object.rs
+++ inter_lang/src/ast/s_inter_lang_object.rs
@@ -1,13 +1,47 @@
-use crate::ast::eKVP;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::Result;
+
+use crate::ast::eValue;
+use crate::ast::sKVP;
+use crate::ast::tItem;
 
 pub struct sInterLangObject {
-    kvps: Vec<eKVP>,
+    kvps: Vec<sKVP>,
 }
 
 impl sInterLangObject {
-    pub fn new() -> Self {
-        Self {
-            kvps: vec![],
+    pub fn new(kvps: Vec<sKVP>) -> Self {
+        Self { kvps }
+    }
+
+    pub fn get(&self, key: &str) -> Option<&eValue> {
+        Some(self.kvps.iter().find(|kvp| kvp.get_key().get_key() == key)?.get_value())
+    }
+}
+
+impl Display for sInterLangObject {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let mut tabs: u8 = 0;
+        let mut as_string: String = "InterLangObject(\n".to_string();
+        
+        {
+            tabs += 1;
+
+            let mut tab_string: String = "".to_string();
+
+            for _ in 0..tabs {
+                tab_string += "\t";
+            }
+
+            for kvp in &self.kvps {
+                as_string += tab_string.as_str();
+                as_string += kvp.as_string(tabs, false).as_str();
+            }
         }
+
+        as_string += ")";
+
+        write!(f, "{}", as_string)
     }
 }
