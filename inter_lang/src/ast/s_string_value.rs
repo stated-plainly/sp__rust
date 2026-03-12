58d48297a5329ff9b906a2b67097014ae457c7a6
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-12 22:13:50 +0000

    casper (lib) :: sStringValue struct added

---
+++ inter_lang/src/ast/s_string_value.rs
@@ -0,0 +1,38 @@
+use crate::ast::tItem;
+use crate::ast::tValue;
+
+pub struct sStringValue {
+    value: String,
+}
+
+impl sStringValue {
+    pub fn new(value: &str) -> Self {
+        Self { value: value.to_string() }
+    }
+
+    pub fn get_value(&self) -> &str {
+        &self.value
+    }
+}
+
+impl tItem for sStringValue {
+    fn as_string(&self, tabs: u8, indent_first_line: bool) -> String {
+        let mut as_string: String = "".to_string();
+
+        if indent_first_line {
+            for _ in 0..tabs {
+                as_string += "\t";
+            }
+        }
+
+        as_string += format!("String({})", self.get_value()).as_str();
+
+        as_string
+    }
+}
+
+impl tValue for sStringValue {
+    fn has_same_composition(&self, other: &Self) -> bool {
+        self.get_value() == other.get_value()
+    }
+}
