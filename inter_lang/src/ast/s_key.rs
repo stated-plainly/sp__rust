554fedf45faeda9b6f5cc8a6bbcf5f3b48f3bc92
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-12 22:06:28 +0000

    casper (lib) :: sKey struct added

--- inter_lang/src/ast/s_key.rs
+++ inter_lang/src/ast/s_key.rs
@@ -1,3 +1,31 @@
-pub struct sKey {
+use crate::ast::tItem;
 
+pub struct sKey {
+    key: String,
+}
+
+impl sKey {
+    pub fn new(key: &str) -> Self {
+        Self { key: key.to_string() }
+    }
+
+    pub fn get_key(&self) -> &str {
+        &self.key
+    }
+}
+
+impl tItem for sKey {
+    fn as_string(&self, tabs: u8, indent_first_line: bool) -> String {
+        let mut as_string = "".to_string();
+
+        if indent_first_line {
+            for _ in 0..tabs {
+                as_string += "\t";
+            }
+        }
+
+        as_string += format!("Key({})", self.get_key()).as_str();
+
+        as_string
+    }
 }
