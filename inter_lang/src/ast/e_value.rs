f06e055eb3916c2e8fdfb2f4519be7346b1fd55f
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-14 01:30:47 +0000

    inter_lang (lib) :: eValue.has_same_composition method logic condensed

--- inter_lang/src/ast/e_value.rs
+++ inter_lang/src/ast/e_value.rs
@@ -21,25 +21,19 @@ impl tASTItem for eValue {
     fn has_same_composition(&self, other: &Self) -> bool {
         match self {
             Self::String(self_string_value) => {
-                if let Self::String(other_string_value) = other {
-                    return self_string_value.has_same_composition(other_string_value);
-                }
+                let Self::String(other_string_value) = other else { return false };
 
-                return false;
+                return self_string_value.has_same_composition(other_string_value);
             }
             Self::List(self_list_value) => {
-                if let Self::List(other_list_value) = other {
-                    return self_list_value.has_same_composition(other_list_value);
-                }
+                let Self::List(other_list_value) = other else { return false };
 
-                return false;
+                return self_list_value.has_same_composition(other_list_value);
             },
             Self::Object(self_object_value) => {
-                if let Self::Object(other_object_value) = other {
-                    return self_object_value.has_same_composition(other_object_value);
-                }
+                let Self::Object(other_object_value) = other else { return false };
 
-                return false;
+                return self_object_value.has_same_composition(other_object_value);
             },
         }
     }
