91203e29307f84477683f08fc43e322547dc5c74
stated-plainly <259430281+stated-plainly@users.noreply.github.com>
2026-03-14 01:06:37 +0000

    syntax (lib) :: lexer file renamed to s_lexer, redundant code removed

--- syntax/src/structs/s_lexer.rs
+++ syntax/src/structs/s_lexer.rs
@@ -24,16 +24,7 @@ impl<I: tIdentifier> sLexer<I> {
     pub fn tokenise(&self, source_code: &str) -> Vec<sToken<I>> {
         let mut output: Vec<sToken<I>> = vec![];
 
-        let mut maybe_skip_to: Option<usize> = None;
-
         for char_index in 0..source_code.len() {
-            if let Some(skip_to) = maybe_skip_to {
-                maybe_skip_to = match skip_to > char_index {
-                    true => continue,
-                    false => None,
-                };
-            }
-
             for lexeme in self.lexemes.iter() {
                 let Some(token) = lexeme.try_tokenise(source_code, char_index) else { continue; };
 
